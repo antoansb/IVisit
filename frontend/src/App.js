9a13bfff2cecffad8bcbda71354615f937706273
Lazar Antonijevic <lakyspider@gmail.com>
2020-02-14 14:23:58 +0100

    Finished MyPlaces & Users' Places

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return ( 
+  return (
     <Router>
       <MainNavigation />
       <main>
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
@@ -21,7 +30,7 @@ const App = () => {
         </Switch>
       </main>
     </Router>
-    );
-}
+  );
+};
 
 export default App;
