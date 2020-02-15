f11ec7692614dd35a698707df400ffdc4023429b
Lazar Antonijevic <lakyspider@gmail.com>
2020-02-15 16:24:30 +0100

    Mongo CRUD for Users

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -2,6 +2,7 @@ const uuid = require('uuid/v4');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
 const DUMMY_USERS = [
     {
@@ -12,41 +13,76 @@ const DUMMY_USERS = [
     }
 ];
 
-const getUsers = (req, res, next) => {
-    res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    }
+    catch (err) {
+        const error = new HttpError('Could not get users.', 500);
+        return next(error);
+    }
+
+    res.json({ users: users.map(user => user.toObject({ getters: true })) });
+
 };
 
-const register = (req, res, next) => {
+const register = async (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        throw new HttpError('Invalid input.' ,422);
+        return next(new HttpError('Invalid input.' ,422)); 
     }
 
-    const {name, email, password } = req.body;
+    const {name, email, password, places } = req.body;
 
-    const hasUser = DUMMY_USERS.find(u => u.email === email);
-    if(hasUser) {
-        throw new HttpError('Email already exists.', 422);
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email:email });
+    }
+    catch(err) {
+        const error = new HttpError('Register failed.', 422);
+        return next(error);
     }
 
-    const createdUser = {
-        id: uuid(),
+    if(existingUser) {
+        const error = new HttpError('User already exists.', 500);
+        return next(error);
+    }
+
+    const createdUser = new User({
         name,
         email,
-        password
-    };
+        image: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/63/97d1d9f934a350cee765c5ac1a466605.jpg/revision/latest/top-crop/width/360/height/360?cb=20190527184444',
+        password,
+        places
+    });
 
-    DUMMY_USERS.push(createdUser);
+    try {
+        await createdUser.save();
+    }
+    catch (err) {
+        const error = new HttpError('Register failed, try again.', 500);
+        return next(error);
+    }
 
-    res.status(201).json({user: createdUser});
+    res.status(201).json({user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
     const { email, password } = req.body;
 
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-    if(!identifiedUser || identifiedUser.password !== password) {
-        throw new HttpError('User not found.', 401);
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    }
+    catch(err) {
+        const error = new HttpError('Login failed.', 500);
+        return next(error);
+    }
+
+    if (!existingUser || existingUser.password !== password) {
+        const error = new HttpError('Cold not login.', 401);
+        return next(error);
     }
     
     res.json({message: 'Logged in.'});
